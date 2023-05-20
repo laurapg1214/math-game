03d0e727bb4228adb1780a043b68a639014c3683
Laura Purcell-Gates <l.purcellgates@gmail.com>
2023-05-20 20:16:15 +0100

    created nums object to store variables

--- math.js
+++ math.js
@@ -1,55 +1,77 @@
 // DOM content loaded
 document.addEventListener('DOMContentLoaded', function() {
   // randomly generate variables
-  let x = Math.floor((Math.random() * 12) + 1);
-  let y = Math.floor((Math.random() * 12) + 1);
-  console.log(x, y);
+  var generateX = () => {
+    let x = Math.floor((Math.random() * 12) + 1);
+    return x;
+  }
+
+  var generateY = () => {
+    let y = Math.floor((Math.random() * 12) + 1);
+    return y;
+  }
 
   // generate question variables and answer
   var calculate = (x, y) => {
     return x + y;
   }
-  
+
   // On #questionButton click, display question
-  var question = document.querySelector('#questionButton');
-  question.onclick = () => {
-    question.innerHTML = x + ' + ' + y;
-  }
+  var question = document.querySelector('#question');
+  var newQuestion = document.querySelector('#newQuestion');
+  newQuestion.onclick = () => {
+    // getNums function as object
+    var getNums = () => {
+      let x = generateX(),
+          y =  generateY(),
+          answer = calculate(x, y);
 
-  // on submit, generate answer; change answer to fixed; hide submit button
-  var submit = document.querySelector('#submit');
-  var input = document.querySelector('#answerText');
-
-  // event listener for enter key
-  input.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-      event.preventDefault();
-      // trigger submit button element with click
-      submit.click()
+      // return values
+      return {x, y, answer};
     }
-  })
+    let nums = getNums();
+    console.log(nums);
+    question.innerHTML = nums.x + ' + ' + nums.y;
+    question.style.fontWeight = 'bold';
+    question.class = '"btn btn-secondary btn-block"';
 
-  // submit button functionality
-  submit.onclick = () => {
-    // run answer function to get correct answer
-    const answer = calculate(x, y);
+    // on submit, generate answer; change answer to fixed; hide submit button
+    var submit = document.querySelector('#submit');
+    var input = document.querySelector('#answerText');
 
-    // grab input, convert to number
-    const response = Number(document.querySelector('#answerText').value);
-    
-    if (answer == response) {
-      correct();
-    } else {
-      incorrect();
+    // event listener for enter key
+    input.addEventListener('keypress', function(event) {
+      if (event.key === 'Enter') {
+        event.preventDefault();
+        // trigger submit button element with click
+        submit.click()
+      }
+    })
+
+    // submit button functionality
+    submit.onclick = () => {
+      // run answer function to get correct answer
+      const answer = calculate(nums.x, nums.y);
+
+      // grab input, convert to number
+      const response = Number(document.querySelector('#answerText').value);
+      
+      if (answer == response) {
+        correct();
+      } else {
+        incorrect();
+      }
     }
-  }
 
-  // correct response function
-  var correct = () => {
-    question.style.backgroundColor = 'green';
-    question.innerHTML = 'Correct!'
-    input.style.backgroundColor = 'green';
-    input.style.color = 'white';
+    // correct response function
+    var correct = () => {
+      question.style.backgroundColor = 'green';
+      question.innerHTML = 'Correct!'
+      question.style.fontWeight = 'normal';
+      input.value = nums.x + ' + ' + nums.y + ' = ' + nums.answer; 
+      input.style.backgroundColor = 'green';
+      input.style.color = 'white';
+    }
   }
     
 
